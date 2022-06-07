07942ca19888f5fe50758a25d3cd63a4181640d6
KingAusti <austin@thehenrys.xyz>
2022-06-07 00:11:33 -0700

    setting up components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
+import About from './components/About'
+import Nav from './components/Nav';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <main>
+        <About>
+        <section className="my-5">
+      <h1 id="about">Who am I?</h1>
+    </section>
+        </About>
+      </main>
+    
   );
 }
 
